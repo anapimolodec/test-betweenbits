1caa60381790490bdb1ee40f649a4d0a1c3e1fa6
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-08-08 14:04:35 +0900

    adding mail logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './images/logo.png';
 import icons from './images/biticons.png';
+import MailLogo from './MailLogo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <div className="header">
-          <img src={icons} className="icons" alt="logo" />
-          <h1>Enterprise Adoption Partner</h1>
+          <img src={icons} className="icons" alt="icons" />
+          <h1>Enterprise Adoption Partnerr</h1>
         </div>
+        <MailLogo />
         <footer>
           <img src={logo} className="logo" alt="logo" />
+
           <p>COMING SOON</p>
           <p>2023</p>
         </footer>
