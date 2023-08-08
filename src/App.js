e4b1fb94634b257f83f35c5705c4655e211df6f2
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-08-08 18:36:00 +0900

    fixed underline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import ComingSoon from './components/ComingSoon';
 import Footer from './components/Footer';
-
-
+import Experiment from './components/Experiment';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
         <ComingSoon />
         <Footer />
-    </div>
+    </div> 
   );
 }
 
---
+++ src/components/Experiment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Experiment = () => {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default Experiment;
